ecec06036f495760a3b931d62c24884f718d25a8
sbishop7 <seth.bishop@gmail.com>
2017-07-12 17:48:01 -0700

    Finished going through custom cells

---
+++ CustomCell/CustomCell.swift
@@ -0,0 +1,18 @@
+//
+//  CustomCell.swift
+//  CustomCell
+//
+//  Created by Seth Bishop on 7/12/17.
+//  Copyright © 2017 Seth Bishop. All rights reserved.
+//
+
+import Foundation
+
+import UIKit
+
+
+class CustomCell: UITableViewCell {
+    @IBOutlet weak var rightLabel: UILabel!
+    @IBOutlet weak var leftButton: UIButton!
+    
+}
---
+++ CustomCell/CustomCellsViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  CustomCell
+//
+//  Created by Seth Bishop on 7/12/17.
+//  Copyright © 2017 Seth Bishop. All rights reserved.
+//
+
+import UIKit
+
+class CustomCellsViewController: UITableViewController {
+    
+    var nums = [1, 90, 32, 23, 9, 12]
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CustomCell") as! CustomCell
+        cell.rightLabel.text = "\(nums[indexPath.row])"
+        if nums[indexPath.row] > 24 {
+            cell.leftButton.backgroundColor = UIColor.green
+        } else {
+            cell.leftButton.backgroundColor = UIColor.red
+        }
+        // return cell so that Table View knows what to draw in each row
+        return cell
+    }
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return nums.count
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
